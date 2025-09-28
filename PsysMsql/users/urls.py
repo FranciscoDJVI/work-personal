e7d3a3a169baba4380401f1f3d3b9d87c62a3c85
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-09-27 20:54:39 -0500

    fix: add generic forms for change password and chage font-size of navbar

--- PsysMsql/users/urls.py
+++ PsysMsql/users/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, PasswordChangeViewUSer
 
-urlpatterns = [path("signup/", SignUpView.as_view(), name="signup")]
+urlpatterns = [
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("password_change/", PasswordChangeViewUSer.as_view(), name="password_change"),
+]
--- PsysMsql/users/views.py
+++ PsysMsql/users/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.contrib.auth.views import PasswordChangeView
 from .forms import SignUpForm
 
 
@@ -9,3 +10,9 @@ class SignUpView(CreateView):
         "login"
     )  # Redirige a la página de login después de registrarse
     template_name = "signup.html"  # Ruta a tu plantilla de registro
+
+
+class PasswordChangeViewUSer(PasswordChangeView):
+    template_name = "password_change_form.html"
+
+    success_url = reverse_lazy("login")
