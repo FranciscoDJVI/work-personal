62612c8179599c781dc03664d7ff7a3934b7aefa
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-09-28 17:04:14 -0500

    change hel_text and laabel to spanish

--- PsysMsql/users/forms.py
+++ PsysMsql/users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth.models import User
 from django import forms
 
@@ -25,14 +25,32 @@ class SignUpForm(UserCreationForm):
                     "class": "bg-gray-200 ",
                 }
             ),
-            "password_1": forms.PasswordInput(
+            "password1": forms.PasswordInput(
                 attrs={
                     "class": "bg-gray-200 ",
                 }
             ),
-            "Password_2": forms.PasswordInput(
+            "Password2": forms.PasswordInput(
                 attrs={
                     "class": "bg-gray-200 ",
                 }
             ),
         }
+
+
+class StyledPasswordChangeForm(PasswordChangeForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["old_password"].label = "Contraseña actual"
+        self.fields["new_password1"].label = "Nueva contraseña"
+        self.fields["new_password2"].label = "Confirmar contraseña"
+
+        self.fields["new_password1"].help_text = ""
+        self.fields[
+            "new_password2"
+        ].help_text = "Escribe de nuevo tu nueva contraseña para verificación."
+
+        self.fields["old_password"].widget.attrs.update({"class": "bg-gray-200 p-2"})
+        self.fields["new_password1"].widget.attrs.update({"class": "bg-gray-200 p-2"})
+        self.fields["new_password2"].widget.attrs.update({"class": "bg-gray-200 p-2"})
--- PsysMsql/users/views.py
+++ PsysMsql/users/views.py
@@ -1,18 +1,18 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.contrib.auth.views import PasswordChangeView
-from .forms import SignUpForm
+from .forms import SignUpForm, StyledPasswordChangeForm
 
 
 class SignUpView(CreateView):
     form_class = SignUpForm
-    success_url = reverse_lazy(
-        "login"
-    )  # Redirige a la página de login después de registrarse
-    template_name = "signup.html"  # Ruta a tu plantilla de registro
+    success_url = reverse_lazy("login")
+    template_name = "signup.html"
 
 
 class PasswordChangeViewUSer(PasswordChangeView):
+    form_class = StyledPasswordChangeForm
+
     template_name = "password_change_form.html"
 
     success_url = reverse_lazy("login")
